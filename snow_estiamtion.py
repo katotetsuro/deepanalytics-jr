1afdb7bc626d5b3ca65353e01642128d68782800
KatoTetsuro <katotetsuro@team-lab.com>
2017-12-21 01:29:42 +0900

    初回submitコードを整理しました

--- snow_estiamtion.py
+++ snow_estiamtion.py
@@ -10,7 +10,7 @@
 # * xgboostの評価指標にweighted maeを追加する（できないかも？）
 # * xgboostのパラメータサーチをhyperoptでがんばる
 
-# In[255]:
+# In[36]:
 
 import pandas as pd
 from glob import glob
@@ -21,19 +21,20 @@ get_ipython().magic('matplotlib inline')
 from datetime import datetime
 from pandas.tseries import offsets
 import math
+from os.path import join
 
 
-# In[3]:
+# In[2]:
 
 csvs = glob('data/*.csv')
 
 
-# In[4]:
+# In[3]:
 
 csvs
 
 
-# In[34]:
+# In[4]:
 
 train = pd.read_csv('data/train.csv') 
 train['年月日'] = train['年月日'].apply(pd.to_datetime)
@@ -41,89 +42,53 @@ test = pd.read_csv('data/test.csv')
 test['年月日'] = test['年月日'].apply(pd.to_datetime)
 
 
-# In[7]:
+# In[5]:
 
 weather = pd.read_csv('data/weather.csv')
 weather['年月日時'] = weather['年月日時'].apply(pd.to_datetime)
 
 
-# In[9]:
+# In[6]:
 
 snowfall = pd.read_csv('data/snowfall.csv')
 snowfall['年月日時'] = snowfall['年月日時'].apply(pd.to_datetime)
 
 
-# In[ ]:
+# In[7]:
 
-snowfall.loc[lambda df: (0 < df['積雪深(軌道)']) & (df['積雪深(軌道)'] < 100), :]
-
-
-# In[156]:
-
-weather['day'] = weather['年月日時'].apply(lambda x: x.dayofyear)
+#zweather['day'] = weather['年月日時'].apply(lambda x: x.dayofyear)
 
 
-# In[193]:
+# In[8]:
 
 s = weather.groupby(['day', '地点'])['気温(℃)'].mean()
 
 
-# In[ ]:
-
-weather.shift()
-
-
-# In[213]:
+# In[9]:
 
 plt.plot(s[:, '富山'], 'b-', alpha=0.5)
 plt.plot(s[:, '金沢'], 'r-', alpha=0.5)
 
 
-# In[212]:
-
-train[ train['停車駅名'] == '金沢']
-
-
-# In[155]:
-
-d.dayofyear
-
-
-# In[132]:
-
-weather.keys()
-
-
-# In[106]:
-
-for k,v in per_train.groups.items():
-    plt.plot(train.loc[v.values]['台車部分'].values, c=np.random.rand(3,))
-
-
-# In[117]:
-
-weather['年月日時']
-
-
-# In[409]:
+# In[12]:
 
 #　とりあえずすごいシンプルに、 { (金沢, 富山) x (1日の平均気温, 1日の合計降水量, 1日の合計降雪量, 1日の合計積雪量, 日照時間合計) } x (おととい、昨日、今日)
 # くらいのデータでxgboostしてみよかな
 train_df = train[train['停車駅名'] == '富山'][['年月日', '列車番号', '台車部分']]
 
 
-# In[415]:
+# In[13]:
 
 month = lambda x: x.month
 train_df = train_df[ (1<=train_df['年月日'].apply(month)) & (train_df['年月日'].apply(month) <= 3) ]
 
 
-# In[416]:
+# In[14]:
 
 len(train_df)
 
 
-# In[417]:
+# In[15]:
 
 # train_dfに足してくぞー
 diagram = pd.read_csv('data/diagram.csv')
@@ -133,52 +98,52 @@ train_df['出発時刻_in_day'] = train_df['出発時刻'].apply(lambda x: x.hou
 train_df = train_df.rename(columns={'年月日':'day'})
 
 
-# In[418]:
+# In[16]:
 
 len(train_df)
 
 
-# In[419]:
+# In[17]:
 
 weather['day'] = weather['年月日時'].apply(lambda x: pd.to_datetime(x.strftime('%Y/%m/%d')))
 
 
-# In[420]:
+# In[18]:
 
 temperature = weather[['気温(℃)','積雪(cm)', '地点', 'day']].groupby(['day', '地点']).mean().reset_index()
 temperature = temperature[ temperature['地点'] != '糸魚川' ]
 temperature = temperature.pivot_table(values=['気温(℃)', '積雪(cm)'], index=['day'], columns=['地点'])
 
 
-# In[421]:
+# In[19]:
 
 len(temperature)
 
 
-# In[422]:
+# In[20]:
 
 features = weather[['降水量(mm)', '降雪(cm)', '日照時間(時間)', '地点', 'day']].groupby(['day', '地点']).sum().reset_index()
 features = features[ features['地点'] != '糸魚川' ]
 features = features.pivot_table(values=['降水量(mm)', '降雪(cm)'], index=['day'], columns=['地点'])
 
 
-# In[423]:
+# In[21]:
 
 len(features)
 
 
-# In[424]:
+# In[22]:
 
 train_df = pd.merge(train_df, features.reset_index(), on=['day'])
 train_df = pd.merge(train_df, temperature.reset_index(), on=['day'])
 
 
-# In[425]:
+# In[23]:
 
 len(train_df)
 
 
-# In[467]:
+# In[24]:
 
 all_keys = train_df.keys()
 train_x_keys = all_keys.drop(['台車部分', 'day', '列車番号', '出発時刻'])
@@ -186,37 +151,37 @@ train_x = train_df[train_x_keys]
 train_y = train_df['台車部分']
 
 
-# In[468]:
+# In[25]:
 
 dm = xgb.DMatrix(train_x, label=train_y)
 
 
-# In[478]:
+# In[26]:
 
 cv = xgb.cv({'eval_metric': 'mae'}, dm, num_boost_round=50)
 
 
-# In[481]:
+# In[27]:
 
 cv.sort_values(by='test-mae-mean')
 
 
-# In[484]:
+# In[28]:
 
 bst = xgb.train({'eval_metric': 'mae'}, dm, num_boost_round=50)
 
 
-# In[488]:
+# In[29]:
 
 len(test)
 
 
-# In[489]:
+# In[30]:
 
 test.keys()
 
 
-# In[495]:
+# In[31]:
 
 test_df = test
 depature_times = pd.to_datetime(diagram[test_df['列車番号']].loc[0])
@@ -227,68 +192,37 @@ test_df = pd.merge(test_df, features.reset_index(), on=['day'])
 test_df = pd.merge(test_df, temperature.reset_index(), on=['day'])
 
 
-# In[496]:
+# In[32]:
 
 test_x = test_df[train_x_keys]
 
 
-# In[498]:
-
-xgb.DMatrix(test_x)
-
-
-# In[501]:
+# In[34]:
 
 predict = bst.predict(xgb.DMatrix(test_x))
 
 
-# In[503]:
+# In[35]:
 
 plt.plot(predict)
 
 
-# In[514]:
+# In[37]:
 
 predict[predict<=0.000007] = 0
 
 
-# In[517]:
+# In[38]:
 
 predict_df = pd.DataFrame(data=predict)
 
 
-# In[519]:
+# In[39]:
 
-predict_df.to_csv('prediction.csv', header=False)
-
-
-# In[520]:
-
-train.describe()
+predict_df.to_csv(join('result', 'prediction.csv'), header=False)
 
 
-# In[521]:
-
-predict_df.describe()
+# In[ ]:
 
 
-# In[499]:
-
-train_x.keys()
-
-
-# In[433]:
-
-ari = train_df[ train_df['台車部分'] > 0 ]
-nashi = train_df[ train_df['台車部分'] == 0 ]
-
-
-# In[436]:
-
-ari.describe()
-
-
-# In[437]:
-
-nashi.describe()
 
